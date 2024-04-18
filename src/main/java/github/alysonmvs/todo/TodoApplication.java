41951914336f813970531946144545dedff4bb44
Alyson <alysonmvs@hotmail.com>
2024-04-17 23:23:44 -0300

    initial project

---
+++ src/main/java/github/alysonmvs/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package github.alysonmvs.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
